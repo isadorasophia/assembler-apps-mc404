233ef68c88a596f2e602eb28c36a686215fc555a
Isadora <isaecia@gmail.com>
2015-08-25 10:56:33 -0300

    Fixed script

--- lab2/correcao-lab2.sh
+++ lab2/correcao-lab2.sh
@@ -6,9 +6,7 @@ IAS_OUT="ias.out"
 # caminho para o executavel do simulador ias
 IAS_EXEC_PATH="./ias"
 
-
 STR_DIV="-------------------------------------------"
-
 
 TEST[0]="105 000000000A"    # X = 10   
 TEST[1]="105 0000000DAC"    # X = 3500 
@@ -16,30 +14,34 @@ TEST[1]="105 0000000DAC"    # X = 3500
 RESULT[0]="0x000000000A"
 RESULT[1]="0x00000000BB"
 
+function clean {
+    rm -f $TMP_FILE $IAS_OUT
+}
+
 if [ $# -lt 1 ]; then
     echo "ERRO!"
     echo "FORMA DE USO:"
     echo " $0 raXXXXXX.hex"
 else
+    clean   # make sure workspace is clean
 
     for((I = 0; I < "${#TEST[@]}"; I++))
     do
-        rm -f $TMP_FILE $IAS_OUT
         touch $TMP_FILE
         touch $IAS_OUT
 
         echo $STR_DIV
         echo "${TEST[$I]}"
 
-        while read line           
-        do             
-            
+        # reads until last line
+        while read line || [ -n "$line" ];
+        do  
             NEW_LINE=`echo "${line%%#*}" | tr -d ' '  `
 
             if [ "${NEW_LINE:0:1}" == '1' ] \
                 && [ "${NEW_LINE:1:1}" == "0" ] \
                 && [ "${NEW_LINE:2:1}" ==  "5" ]; then
-                echo ${TEST[$I]}  >> $TMP_FILE
+                continue    # ignore line
 
             elif [ "${NEW_LINE}" != "" ]; then
                 NEW_LINE="${NEW_LINE:0:1}${NEW_LINE:1:1}${NEW_LINE:2:1} ${NEW_LINE:3}"
@@ -47,6 +49,7 @@ else
             fi
         done < $1
 
+        echo ${TEST[$I]} >> $TMP_FILE  # add test line
 
         $IAS_EXEC_PATH --load=$TMP_FILE >& $IAS_OUT
 
@@ -60,11 +63,13 @@ else
 
         if [ "$STR_AC" == "${RESULT[$I]}" ] \
             && [ "$STR_PC" == "0x0000000400" ]; then
-            echo "CORRETO"        
+            echo "CORRETO"
         else
             echo "ERRO"
         fi
 
         echo $STR_DIV
+
+        clean
     done
 fi
