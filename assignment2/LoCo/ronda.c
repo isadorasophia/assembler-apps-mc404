cb3b275aa7ef336098e3a185405754316cb3f3ae
Isadora Sophia <saecia@gmail.com>
2015-11-23 18:13:26 -0200

    Added assignment 2

---
+++ assignment2/LoCo/ronda.c
@@ -0,0 +1,96 @@
+/* ********************************************************************* *
+ * 
+ *                                 Ronda
+ * Author: Isadora Sophia Garcia Rodopoulos
+ * RA: 158018
+ * Description: Uoli's control logic as "Ronda"
+ *
+ * ********************************************************************* */
+
+#include <stdio.h>
+#include "../BiCo/api_robot2.h"
+
+#define uint unsigned int
+
+#define DEFAULT_SPEED  25      /* Default speed */
+#define MIN_INTERVAL   15      /* Min. interval to turn 90 degrees */
+
+#define MAX_UNITY      50      /* Max. unit of time before reset */
+#define MIN_DISTANCE   1200    /* Min. distance to a wall */
+
+#define LEFT_SONAR_ID  3
+#define RIGHT_SONAR_ID 4
+
+#define true  1                /* Bool */
+#define false 0
+
+uint total_time;
+
+int main() {
+    /* Initialize timer */
+    total_time = 1;
+
+    /* Set alarm */
+    set_alarm(f);
+
+    /* Set proximity callbacks */
+    registry_proximity_callback(LEFT_SONAR_ID, MIN_DISTANCE, &near_wall_left);
+    registry_proximity_callback(RIGHT_SONAR_ID, MIN_DISTANCE, &near_wall_right);
+
+    /* Go! */
+    forward();
+
+    return 0;
+}
+
+/* Function that will be called when the timer is up */
+void timer() {
+    turn_90_right();
+
+    total_time++;
+
+    /* Make sure unit is accordingly */
+    if (total_time > MAX_UNITY) {
+        total_time = 1;
+    }
+
+    set_alarm();
+}
+
+void turn_90_right() {
+    uint i, t;
+
+    i = get_time();
+
+    set_motors_speed(DEFAULT_SPEED/2, 0);
+
+    t = get_time();
+
+    /* Keeps turning until 90 degrees was made 
+     * according to MIN_INTERVAL */
+    while (t - i < MIN_INTERVAL) {
+        t = get_time();
+    }
+
+    forward();
+}
+
+void forward() {
+    set_motors_speed(DEFAULT_SPEED, DEFAULT_SPEED);
+}
+
+void set_alarm() {
+    add_alarm(&timer, total_time);
+}
+
+void near_wall_left() {
+    turn_90_right();
+
+    registry_proximity_callback(LEFT_SONAR_ID, MIN_DISTANCE, &near_wall_left);
+}
+
+void near_wall_right() {
+    turn_90_right();
+
+    registry_proximity_callback(RIGHT_SONAR_ID, MIN_DISTANCE, &near_wall_right);
+}
---
+++ assignment2/LoCo/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "api_robot2.h"
+
+int main () {
+    printf("oi mundo");
+
+    return 0;
+}
